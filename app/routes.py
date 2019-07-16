9c2c63a0b87f12836adf5de72cf206625244ee6c
istteo <istteogit@gmail.com>
2019-07-16 18:47:05 +0100

    Add JSON files with time tables and pass them to index

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,18 @@
 from app import app
 from flask import render_template
+import os
+import json
+
 @app.route('/')
 def index():
-	return render_template('index.html')
+	boatFile = os.path.abspath(os.path.dirname(__file__) + '/boatTable.json')
+	with open(boatFile) as infile:
+		boatTable =  json.load(infile)
+	boatStr = json.dumps(boatTable, separators=(',',':'))
+	
+	metroFile = os.path.abspath(os.path.dirname(__file__) + '/metroTable.json')
+	with open(metroFile) as infile:
+		metroTable =  json.load(infile)
+	metroStr = json.dumps(metroTable, separators=(',',':'))
+
+	return render_template('index.html', metro=metroStr, boat=boatStr)
