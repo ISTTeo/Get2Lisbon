43e1a891da5ae596703d87b23038a034d679a12b
istteo <istteogit@gmail.com>
2019-07-18 16:45:32 +0100

    Add routes that return JSON with boat and metro timetables. Sample index redirecting to the json routes

--- app/routes.py
+++ app/routes.py
@@ -5,14 +5,25 @@ import json
 
 @app.route('/')
 def index():
-	boatFile = os.path.abspath(os.path.dirname(__file__) + '/boatTable.json')
-	with open(boatFile) as infile:
-		boatTable =  json.load(infile)
-	boatStr = json.dumps(boatTable, separators=(',',':'))
+	return render_template('index.html')
 	
+@app.route('/metro')
+def getMetro():
+
 	metroFile = os.path.abspath(os.path.dirname(__file__) + '/metroTable.json')
 	with open(metroFile) as infile:
 		metroTable =  json.load(infile)
 	metroStr = json.dumps(metroTable, separators=(',',':'))
 
-	return render_template('index.html', metro=metroStr, boat=boatStr)
+	#return render_template('index.html', metro=metroStr, boat=boatStr)
+	return metroTable
+
+@app.route('/boat')
+def getBoat():
+	boatFile = os.path.abspath(os.path.dirname(__file__) + '/boatTable.json')
+	with open(boatFile) as infile:
+		boatTable =  json.load(infile)
+	boatStr = json.dumps(boatTable, separators=(',',':'))
+
+	#return render_template('index.html', metro=metroStr, boat=boatStr)
+	return boatTable
